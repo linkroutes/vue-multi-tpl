f212bbd16f9879b0cfd435d2aca113d9975d4050
kai@home <chanthinker@foxmail.com>
2021-06-03 22:40:42 +0800

    no message

--- src/pages/index/App.vue
+++ src/pages/index/App.vue
@@ -15,10 +15,38 @@
   import Toast from 'vant/lib/toast';
   import 'vant/lib/toast/style';
 
+  const pageUrl = location.href;
+  let _title = document.title;
+  let _desc = "自定义描述"
+  let _pureLink = pageUrl.split('?')[0]
+  
   export default {
     name:'pageIndex',
     data(){
       return {
+        root:( pageUrl.indexOf('t-csbj')>-1 || pageUrl.indexOf('thinkerchan')>-1 || pageUrl.indexOf(':808')> - 1 ) ? 'https://t-csbj.linkroutes.com/api/' : 'https://csbj.linkroutes.com/api/',
+        auth:'wx/authorize',
+        signature:'wx/signature',
+        userInfo:'user/info',
+        testMode:false,
+        isWeixin: navigator.userAgent.toLowerCase().indexOf('micromessenger')>-1,
+        shareData:{ // 分享数据
+          title: _title,
+          desc: _desc,
+          link: _pureLink,
+          imgUrl: 'https://www1.pcbaby.com.cn/test/gz/202101271000/img/cover.jpg',
+          success: (data)=> {
+            console.log('success',data);
+            // window._czc &&  window._czc.push(["_trackEvent", "shareSuccess", "微信分享成功"]);
+          },
+          cancel: (data)=>{
+            console.log('cancel:',data);
+            // window._czc &&  window._czc.push(["_trackEvent", "shareFailed", "微信分享取消"]);
+          }
+        },
+        openid:'',
+        token:'',
+
         loadingTxt:0,
       }
     },
