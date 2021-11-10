27e48014da3a6d1b2b4d28c95cca2fe411769e08
chenxinkai <chenxinkai@pconline.com.cn>
2021-11-10 11:22:40 +0800

    update

--- src/pages/index/App.vue
+++ src/pages/index/App.vue
@@ -1,82 +1,262 @@
 <template>
   <div class="page-index">
     首页
-    <p>{{loadingTxt}}</p>
-    <br/>
-    <br/>
-    <img src="" class="avatar">
+    <p>{{ loadingTxt }}</p>
+    <br />
+    <br />
+    <img src="" class="avatar" />
   </div>
 </template>
 
 <script>
-  import axios from 'axios'
-  import qs from 'qs'
+import axios from "axios";
+import qs from "qs";
 
-  import Toast from 'vant/lib/toast';
-  import 'vant/lib/toast/style';
+import Toast from "vant/lib/toast";
+import "vant/lib/toast/style";
 
-  const pageUrl = location.href;
-  let _title = document.title;
-  let _desc = "自定义描述"
-  let _pureLink = pageUrl.split('?')[0]
-  
-  export default {
-    name:'pageIndex',
-    data(){
-      return {
-        root:( pageUrl.indexOf('t-csbj')>-1 || pageUrl.indexOf('thinkerchan')>-1 || pageUrl.indexOf(':808')> - 1 ) ? 'https://t-csbj.linkroutes.com/api/' : 'https://csbj.linkroutes.com/api/',
-        auth:'wx/authorize',
-        signature:'wx/signature',
-        userInfo:'user/info',
-        testMode:false,
-        isWeixin: navigator.userAgent.toLowerCase().indexOf('micromessenger')>-1,
-        shareData:{ // 分享数据
-          title: _title,
-          desc: _desc,
-          link: _pureLink,
-          imgUrl: 'https://www1.pcbaby.com.cn/test/gz/202101271000/img/cover.jpg',
-          success: (data)=> {
-            console.log('success',data);
-            // window._czc &&  window._czc.push(["_trackEvent", "shareSuccess", "微信分享成功"]);
-          },
-          cancel: (data)=>{
-            console.log('cancel:',data);
-            // window._czc &&  window._czc.push(["_trackEvent", "shareFailed", "微信分享取消"]);
-          }
+const PAGEURL = location.href;
+const TESTMODE =
+  PAGEURL.indexOf("testMode=true") > -1 ||
+  PAGEURL.indexOf("localhost") > -1 ||
+  PAGEURL.indexOf(":808") > -1;
+
+const docEle = document.documentElement;
+const dclientWidth = docEle.clientWidth;
+const dclientHeight = docEle.clientHeight;
+
+let _title = document.title;
+let _desc = "自定义描述";
+let _pureLink = PAGEURL.split("?")[0];
+
+export default {
+  name: "pageIndex",
+  data() {
+    return {
+      loadingTxt: 0,
+      pageScale: 1,
+      cssText: "",
+      testMode: false,
+      // 接口
+      root:
+        PAGEURL.indexOf("//csbj") > -1
+          ? "https://csbj.linkroutes.com/api/"
+          : "https://t-csbj.linkroutes.com/api/",
+      auth: "wx/authorize",
+      signature: "wx/signature",
+      userInfo: "user/info",
+      isWeixin:
+        navigator.userAgent.toLowerCase().indexOf("micromessenger") > -1,
+      qrcodeRoot: "https://qrcode.jp/qr?q=",
+      imgBase64api: "common/getBase64Img?url=",
+      shareData: {
+        title: _title,
+        desc: _desc,
+        link: _pureLink,
+        imgUrl:
+          "https://www1.pcbaby.com.cn/test/gz/project/img/20210904/cover.jpg",
+        success: (data) => {
+          window._czc &&
+            window._czc.push([
+              "_trackEvent",
+              "shareSuccess",
+              "微信分享成功",
+              this.SRC,
+            ]);
         },
-        openid:'',
-        token:'',
+        cancel: (data) => {
+          window._czc &&
+            window._czc.push([
+              "_trackEvent",
+              "shareFailed",
+              "微信分享取消",
+              this.SRC,
+            ]);
+        },
+      },
+      openId: "",
+      token: "",
+      sid: "",
+      username: "测试狗",
+      avatar: "https://img01.yzcdn.cn/vant/cat.jpeg",
+      pageIndex: 0,
+    };
+  },
+  created() {
+    this.testMode = TESTMODE;
+  },
+  mounted() {
+    let urlData = this.$tool.parseURL(location.href).params;
+    console.log("页面携带的参数：", urlData);
+    this.openId = urlData.openId;
+    this.token = urlData.token;
+    this.sid = urlData.sid;
 
-        loadingTxt:0,
-      }
-    },
-    created(){
+    // 进入页面首选检查授权
+    this.checkAuth(this.openId, this.token);
+
+    this.fixPage(true);
+
+    this.init(()=>{
+      // 加载完毕显示页面
+    })
+
+    this.setShare(() => {
+      // this.Jbgm.play(); // 按需音频加载
+    });
+
+  },
+  methods: {
+    init(cb) {
       this.$tool.preload({
-        list:[
-          {src:require("../images/hi.png")},
-        ],
-        progress:(percent)=>{
+        list: [{ src: require("../images/hi.png") }],
+        progress: (percent) => {
           this.loadingTxt = percent;
         },
-        success:()=>{
-          Toast('加载完毕!')
+        success: () => {
+          Toast("加载完毕!");
+          let t = setTimeout(() => {
+            cb && cb();
+            clearTimeout(t);
+            t = null;
+          }, 1000);
+        },
+      });
+    },
+    fixPage(force) {
+      if (force) {
+        let rootEle = document.documentElement;
+        window.pageRadio = +(
+          rootEle.clientWidth / rootEle.clientHeight
+        ).toFixed(2);
+        console.log("强制fixPage(pageRadio)：", window.pageRadio);
+      }
+      if (window.pageRadio > window.psdRadio) {
+        this.pageScale = +(window.psdRadio / window.pageRadio).toFixed(2);
+        this.cssText = `transform:scale(${this.pageScale});transform-origin: 50% 0 0 `;
+      }
+    },
+    go2auth(url) {
+      console.log("跳转前的url:", url);
+      let target =
+        this.root + this.auth + "?redirect=" + encodeURIComponent(url);
+      location.replace(target);
+    },
+    checkAuth(openId, token) {
+      if (this.testMode) {
+        console.log(
+          "testMode:",
+          this.testMode,
+          " 不做网页授权跳转或获取微信用户信息"
+        );
+        return;
+      }
+      console.log("是否有openId:", openId);
+      if (openId && this.isWeixin) {
+        this.getUserInfo(openId, token);
+        window._czc &&
+          window._czc.push(["_trackEvent", "pageView", "展示页面", this.SRC]);
+      } else {
+        this.go2auth(location.href);
+      }
+    },
+    getUserInfo(openId, token) {
+      if (this.testMode) {
+        console.log("testMode:", this.testMode, " 不请求用户微信数据接口");
+        return;
+      }
+      axios({
+        url: this.root + this.userInfo,
+        params: {
+          openId,
+          token,
         },
       })
+        .then((data) => {
+          let rawData = data.data,
+            code = rawData.code,
+            res = rawData.data;
+          if (!code) {
+            console.log("微信数据:", res);
+            this.username = res.nickname;
+            this.avatar = res.headImgUrl;
+
+            this.fixPage(true);
+          } else {
+            console.log(rawData);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
-    methods:{
-      
-    }
-  }
+    setShare(cb) {
+      if (this.testMode) {
+        console.log("testMode:", this.testMode, " 不请求微信signature");
+        return;
+      }
+      axios({
+        method: "post",
+        url: this.root + this.signature,
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        transformRequest: [
+          (data) => {
+            data = qs.stringify(data);
+            return data;
+          },
+        ],
+        data: {
+          url: encodeURIComponent(location.href.split("#")[0]),
+        },
+      })
+        .then((data) => {
+          let rawData = data.data,
+            code = rawData.code,
+            res = rawData.data;
+          if (!code) {
+            this.registerWx(res, () => {
+              cb && cb();
+            });
+          } else {
+            console.log(rawData);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    registerWx(data, cb) {
+      let win = window;
+      if (win.wx) {
+        win.wx.config({
+          debug: false,
+          appId: data.appId,
+          timestamp: data.timestamp,
+          nonceStr: data.nonceStr,
+          signature: data.signature,
+          jsApiList: [
+            "checkJsApi",
+            "onMenuShareTimeline",
+            "onMenuShareAppMessage",
+          ],
+        });
+        win.wx.ready(() => {
+          win.wx.onMenuShareAppMessage(this.shareData);
+          win.wx.onMenuShareTimeline(this.shareData);
+          cb && cb();
+        });
+        win.wx.error((res) => {
+          console.log(res.errMsg);
+        });
+      }
+    },
+  },
+};
 </script>
 
 <style lang="scss" >
-  @import '@/assets/scss/app.scss';
-  .page-index{
-    .avatar{
-      width: 2rem;
-      height: 2rem;
-      @include gPlaceholder();
-      margin: 0 auto;
-    }
-  }
+@import "@/assets/scss/app.scss";
+@import "./page.scss";
 </style>
--- src/utils/tool.js
+++ src/utils/tool.js
@@ -11,4 +11,45 @@ export default {
       config.success && config.success()
     });
   },
+  parseURL(url) {
+    let a = document.createElement('a');
+    a.href = url;
+    return {
+      source: url,
+      protocol: a.protocol.replace(':', ''),
+      host: a.host,
+      hostname: a.hostname,
+      port: a.port,
+      query: a.search,
+      params: (() => {
+        let ret = {},
+          seg = a.search.replace(/^\?/, '').split('&'),
+          len = seg.length,
+          i = 0,
+          s;
+        for (; i < len; i++) {
+          if (!seg[i]) {
+            continue;
+          }
+          s = seg[i].split('=');
+          ret[s[0]] = s[1];
+        }
+        return ret;
+      })(),
+      file: (a.pathname.match(/\/([^\/?#]+)$/i) || [, ''])[1],
+      hash: a.hash.replace('#', ''),
+      path: a.pathname.replace(/^([^\/])/, '/$1'),
+      relative: (a.href.match(/tps?:\/\/[^\/]+(.+)/) || [, ''])[1],
+      segments: a.pathname.replace(/^\//, '').split('/')
+    };
+  },
+  getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i].trim();
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+  }
 }
